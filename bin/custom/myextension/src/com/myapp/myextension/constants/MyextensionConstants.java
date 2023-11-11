4ef9dc9a8a507506776ba10bde1c4b5c2f21310b
glebgol <glebgolovnev2004@gmail.com>
2023-11-11 21:01:30 +0300

    Connection between 2 extensions

---
+++ bin/custom/myextension/src/com/myapp/myextension/constants/MyextensionConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.myapp.myextension.constants;
+
+/**
+ * Global class for all Myextension constants. You can add global constants for your extension into this class.
+ */
+
+public final class MyextensionConstants extends GeneratedMyextensionConstants
+{
+	public static final String EXTENSIONNAME = "myextension";
+
+	private MyextensionConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
