f5d1f5043c56571e1aa9b559b2a15d2d4b523cdb
glebgol <glebgolovnev2004@gmail.com>
2023-11-23 22:03:43 +0300

    Add a dynamic attribute for an item

---
+++ bin/custom/concerttours/src/concerttours/handlers/DynamicAttributesStringSample.java
@@ -0,0 +1,30 @@
+package concerttours.handlers;
+
+import de.hybris.platform.servicelayer.model.attribute.DynamicAttributeHandler;
+import de.hybris.tutorial.model.ClientNameModel;
+
+public class DynamicAttributesStringSample implements DynamicAttributeHandler<String, ClientNameModel>
+{
+    public static final String VALUE_DELIMITER = " ";
+
+    @Override
+    public String get(final ClientNameModel item)
+    {
+        if (item == null)
+        {
+            throw new IllegalArgumentException("Item model is required");
+        }
+        return item.getFirstName() + VALUE_DELIMITER + item.getLastName();
+    }
+
+    @Override
+    public void set(final ClientNameModel item, final String value)
+    {
+        if (item != null && value != null)
+        {
+            final String[] split = value.split(VALUE_DELIMITER);
+            item.setFirstName(split[0]);
+            item.setLastName(split[1]);
+        }
+    }
+}
