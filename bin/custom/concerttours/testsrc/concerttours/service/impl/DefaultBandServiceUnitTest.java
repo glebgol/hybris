3458110859ecebc6bf74d35e117ca198a327d5c7
glebgol <glebgolovnev2004@gmail.com>
2023-11-27 10:54:58 +0300

    Add localized attribute to an item

--- bin/custom/concerttours/testsrc/concerttours/service/impl/DefaultBandServiceUnitTest.java
+++ bin/custom/concerttours/testsrc/concerttours/service/impl/DefaultBandServiceUnitTest.java
@@ -10,6 +10,7 @@
  * terms of the license agreement you entered into with SAP Hybris.
  */
 package concerttours.service.impl;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -17,6 +18,8 @@ import de.hybris.bootstrap.annotations.UnitTest;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
+
 import org.junit.Before;
 import org.junit.Test;
 import concerttours.daos.BandDAO;
@@ -56,7 +59,7 @@ public class DefaultBandServiceUnitTest
         bandModel.setCode(BAND_CODE);
         bandModel.setName(BAND_NAME);
         bandModel.setAlbumSales(1000L);
-        bandModel.setHistory(BAND_HISTORY);
+        bandModel.setHistory(BAND_HISTORY, Locale.ENGLISH);
     }
     /**
      * This test tests and demonstrates that the Service's getAllBands method calls the DAOs' getBands method and returns
